0d58f2400c14087ec8e12e73d28a680ca38d0712
drnxv <87783549+drnxv@users.noreply.github.com>
2022-05-27 15:35:13 -0400

    finished queue and stack files

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,5 +1,81 @@
 /* Queue implementation using linked nodes */
 
 public class MyQueue {
-  
+  private Node head;
+
+  MyQueue() {
+    head = null;
+  }
+
+  private class Node {
+    private int data;
+    private Node next;
+
+    Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  // add nodes to the front of the queue
+  public void enqueue(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = newNode;
+    } else {
+      newNode.next = head;
+      head = newNode;
+    }
+  }
+
+  // removes the end of the queue since that was the first node added
+  public int dequeue() {
+    int res = -1;
+    Node curr;
+
+    if (head != null) {
+      curr = head;
+      while (curr.next.next != null) {
+        curr = curr.next;
+      }
+
+      Node last = curr.next;
+      res = last.data;
+      curr.next = null;
+      last = null;
+    }
+
+    return res;
+  }
+
+  public boolean isEmpty() {
+    return head == null;
+  }
+
+  public String toString() {
+    String res = "";
+    Node curr = head;
+
+    if (head != null) {
+      while (curr != null) {
+        res += curr.data + " ";
+        curr = curr.next;
+      }
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) {
+    MyQueue queue = new MyQueue();
+    System.out.println(queue.isEmpty());
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+    queue.enqueue(5);
+    queue.dequeue();
+    System.out.println(queue.isEmpty());
+    System.out.println(queue);
+  }
 }
--- src/MyStack.java
+++ src/MyStack.java
@@ -17,6 +17,7 @@ public class MyStack {
     }
   }
 
+  // adds a node to the top of the stack 
   public void push(int data) {
     Node newNode = new Node(data);
     if (top == null) {
@@ -31,13 +32,57 @@ public class MyStack {
     }
   }
 
+  // removes the most recently added node 
   public int pop() {
-    int res = top.data;
-    top = top.next;
+    int res = -1;
+    Node curr;
+
+    if (top != null) {
+      if (top.next != null) {
+        curr = top;
+        while (curr.next.next != null) {
+          curr = curr.next;
+        }
+
+        Node last = curr.next;
+        res = last.data;
+        curr.next = null;
+        last = null;
+      } else {
+        // clear the single element
+        top = null;
+      }
+    }
+
     return res;
   }
 
   public boolean isEmpty() {
     return top == null;
+  }
+
+  public String toString() {
+    String res = "";
+    Node curr = top;
+    while (curr != null) {
+      res += curr.data + " ";
+      curr = curr.next;
+    }
+
+    return res;
+  }
+
+  // showing the functionalities of the stack
+  public static void main(String[] args) {
+    MyStack stack = new MyStack();
+    System.out.println(stack.isEmpty());
+    stack.push(12);
+    stack.push(13);
+    stack.push(14);
+    stack.push(15);
+    stack.push(16);
+    stack.pop();
+    System.out.println(stack.isEmpty());
+    System.out.println(stack);
   }
 }
